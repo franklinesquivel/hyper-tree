b24f6a62d0fa42d49b79076f7a32e71bc9f1c0a2
Zelda Zamora <zeldazamora2@gmail.com>
2020-09-18 13:03:55 -0500

    Adds bin exec, modifies some things in tsconfig.json.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+#!/usr/bin/env node 
+//nodemon --watch 'src/**/*.ts' --exec 'ts-node' src/index.ts
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import program from "commander";
+
+clear();
+console.log(
+  chalk.cyanBright(
+    figlet.textSync('hyper-tree', { horizontalLayout: 'full' })
+  )
+);
+
+// For new flags just follow the next template, .option('-{shorthand}, --{long_name}', '{Help for flag}')
+program
+  .version('1.2.0')
+  .description("A CLI for obtaining files in a directory")
+  .option('-t, --test', 'Test command')
+  .parse(process.argv);
+
+// Put new code for other things below this comment
+
 import testFunction from "./module";
 
-console.log("Hello World!", testFunction(5854));
+// If the user used a flag, it will count as boolean, in a future I will research for passing text with flags
+if (program.test) console.log("The CLI is up an running!", testFunction(5854));
